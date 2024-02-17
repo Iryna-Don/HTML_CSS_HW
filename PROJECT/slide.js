6e554b0494852163db8b0109f945e349bea48601
Iryna-Don <idonets0104081518@gmail.com>
2024-02-17 14:13:16 +0100

    added slide.js

--- PROJECT/slide.js
+++ PROJECT/slide.js
@@ -1,19 +1,25 @@
-
-    if (document.documentElement.clientWidth < 500) {
-        let myIndex = 0;
+window.addEventListener("resize", function(){
+    // location.reload();
+    if (window.screen.width <= 500) {
+        let counterIndex = 0;
         slider();
-
         function slider() {
             let i;
             let x = document.getElementsByClassName("slide");
             for (i = 0; i < x.length; i++) {
                 x[i].style.display = "none";
             }
-            myIndex++;
-            if (myIndex > x.length) {
-                myIndex = 1
+            counterIndex++;
+            if (counterIndex > x.length) {
+                counterIndex = 1
             }
-            x[myIndex - 1].style.display = "block";
-            setTimeout(slider, 2000);
+            x[counterIndex - 1].style.display = "block";
+            setTimeout(slider, 2200);
         }
     }
+}, false);
+
+window.addEventListener("resize", function(){
+        if (window.screen.width > 500) {
+        location.reload()}}, false)
+
